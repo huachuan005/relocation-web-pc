810be2f6c646a50321f8c2b66068c70e623a1eaa
huachuan005 <hc0505@126.com>
2019-12-30 10:37:53 +0800

    1.删除icon

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'src/common/style/index.less'
-import Element from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+// import Element from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.use(Element, { size: 'medium', zIndex: 3000, fontSize: '14' })
+// Vue.use(Element, { size: 'medium', zIndex: 3000, fontSize: '14' })
 
 // 阻止启动生产消息
 Vue.config.productionTip = false
